3659fb734fb65d04ea93863e2a3e05d13c1cad5b
dqrkto <84582260+dqrkto@users.noreply.github.com>
2021-09-24 13:30:40 -0300

    Add files via upload

---
+++ js/in/scripts.js
@@ -0,0 +1,48 @@
+function findLinks(sitesJSON) {
+	let modalContent = [];
+
+	for (oneColumn in sitesJSON) {
+		for (fav of sitesJSON[oneColumn].content) {
+			if (typeof fav.link == 'object') {
+				modalContent.push(fav);
+
+				for (link of fav.link) {
+					if (typeof link.link == 'object') {
+						modalContent.push(link);
+					}
+				}
+			}
+		}
+	}
+
+	return modalContent;
+}
+
+const templateBox = Handlebars.compile(
+	document.getElementById('boxAll').innerHTML
+);
+document.getElementById('boxAll').innerHTML = templateBox({
+	sitesJSON: sitesJSON,
+});
+
+const templateModal = Handlebars.compile(
+	document.getElementById('modalAll').innerHTML
+);
+document.getElementById('modalAll').innerHTML = templateModal({
+	content: findLinks(sitesJSON),
+});
+
+var toggleBol = true;
+
+function toggleTheme() {
+	var elements = document.getElementsByClassName('toggleTheme');
+
+	toggleBol = !toggleBol;
+
+	const toggleValues = toggleBol ? ['dark', 'light'] : ['light', 'dark'];
+
+	for (element of elements) {
+		element.classList.add(toggleValues[0]);
+		element.classList.remove(toggleValues[1]);
+	}
+}
