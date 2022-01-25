1581437e63ae80789a22a503ba05aa548d66b68a
Lucas Prado <pradoplucas@gmail.com>
2022-01-25 11:32:39 -0300

    newSites

---
+++ js/sites.js
@@ -0,0 +1,816 @@
+const sitesJSON = {
+	"utf": {
+		"name": "UTF",
+		"box": "col-sm-3 col-md-2",
+		"card": "col-sm-12",
+		"content": [
+			{
+				"name": "Portal",
+				"link": "https://sistemas2.utfpr.edu.br/login?returnUrl=%2Fdpls%2Fsistema%2Faluno02%2Fmpmenu.inicio"
+			},
+			{
+				"name": "Moodle",
+				"link": "https://moodle.utfpr.edu.br/"
+			},
+			{
+				"name": "Drive UTF",
+				"link": "https://drive.google.com/drive/u/1/"
+			},
+			{
+				"name": "Gmail UTF",
+				"link": "https://mail.google.com/mail/u/1/"
+			},
+			{
+				"name": "Drive Key",
+				"link": "https://drive.google.com/drive/u/1/folders/1uAs5juoO6WiSgM513839BV_9BIu_KNqL"
+			},
+			{
+				"nick": "maisUtf",
+				"name": "maisUTF",
+				"link": [
+					{
+						"name": "CertificaUTF",
+						"link": "https://certificautf.herokuapp.com/"
+					},
+					{
+						"name": "SGCE",
+						"link": "http://apl.utfpr.edu.br/extensao/certificados/listaPublica"
+					},
+					{
+						"name": "Cursos",
+						"link": "http://www.utfpr.edu.br/cursos/graduacao"
+					},
+					{
+						"nick": "ludico",
+						"name": "LUDICO",
+						"link": [
+							{
+								"name": "Facebook",
+								"link": "https://www.facebook.com/UTFPR.BG"
+							},
+							{
+								"name": "Instagram",
+								"link": "https://www.instagram.com/utfprbg/"
+							},
+							{
+								"name": "Blog",
+								"link": "http://utfprbg.cp.utfpr.edu.br/"
+							},
+							{
+								"name": "YouTube",
+								"link": "https://www.youtube.com/c/UTFPRBG"
+							},
+							{
+								"name": "Drive",
+								"link": "https://drive.google.com/drive/u/1/folders/14GtqH23Y-_fvUc2zdBM8ecDRRsh9yvai"
+							},
+							{
+								"name": "Miro",
+								"link": "https://miro.com/app/board/o9J_l7zVQv4=/"
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+
+	"favs": {
+		"nick": "favs",
+		"name": "Favs",
+		"box": "col-sm-6 col-md-8",
+		"card": "col-sm-6 col-md-3",
+		"content": [
+			{
+				"name": "GNews",
+				"link": "https://news.google.com/"
+			},
+			{
+				"name": "globo.com",
+				"link": "http://globo.com/"
+			},
+			{ 	"name": "Twitch", 
+				"link": "http://twitch.tv/" 
+			},
+			{ 	"name": "HLTV.org", 
+				"link": "http://hltv.org/" 
+			},
+			{
+				"nick": "message",
+				"name": "Messenger",
+				"link": [
+					{
+						"name": "WhatsApp",
+						"link": "http://web.whatsapp.com/"
+					},
+					{
+						"name": "Telegram",
+						"link": "http://web.telegram.org/"
+					}
+				]
+			},
+			{
+				"name": "Spotify",
+				"link": "https://open.spotify.com/"
+			},
+			{
+				"nick": "init",
+				"name": "Init",
+				"link": [
+					{
+						"name": "VLC",
+						"link": "https://www.videolan.org/"
+					},
+					{
+						"name": "git",
+						"link": "https://git-scm.com/"
+					},
+					{
+						"name": "VSCode",
+						"link": "https://code.visualstudio.com/"
+					},
+					{
+						"name": "qBitTorrent",
+						"link": "https://www.qbittorrent.org/"
+					},
+					{
+						"name": "Chrome",
+						"link": "https://www.google.com/chrome/"
+					},
+					{
+						"name": "Inkscape",
+						"link": "https://inkscape.org/"
+					},
+					{
+						"name": "Photoscape",
+						"link": "http://www.photoscape.org/ps/main/index.php"
+					},
+					{
+						"name": "Python",
+						"link": "https://www.python.org/downloads/"
+					},
+					{
+						"name": "Sublime",
+						"link": "https://www.sublimetext.com/"
+					}
+				]
+			},
+			{
+				"nick": "social",
+				"name": "Social",
+				"link": [
+					{
+						"name": "Facebook",
+						"link": "https://www.facebook.com/"
+					},
+					{
+						"name": "Instagram",
+						"link": "https://www.instagram.com/"
+					},
+					{
+						"name": "Twitter",
+						"link": "https://twitter.com/"
+					},
+					{
+						"name": "Discord",
+						"link": "https://discord.com/app"
+					},
+
+				]
+			},
+			{
+				"name": "Overleaf",
+				"link": "http://overleaf.com/"
+			},
+			{
+				"nick": "orders",
+				"name": "Orders",
+				"link": [
+					{
+						"name": "Shopee",
+						"link": "http://shopee.com/"
+					},
+					{
+						"name": "AliExpress",
+						"link": "https://www.aliexpress.com/"
+					},
+					{
+						"name": "Correios",
+						"link": "https://www2.correios.com.br/sistemas/rastreamento/"
+					}
+				]
+			},
+			{
+				"nick": "nba",
+				"name": "NBA",
+				"link": [
+					{
+						"name": "Games",
+						"link": "https://www.nba.com/games"
+					},
+					{
+						"name": "yt - NBA BR",
+						"link": "https://www.youtube.com/channel/UCsiCOcr5SZAWlJMfcw9P__w"
+					}
+				]
+			},
+			{
+				"nick": "original",
+				"name": "!Original",
+				"link": [
+					{
+						"name": "Futemax",
+						"link": "http://futemax.live/"
+					},
+					{
+						"name": "Multicanais",
+						"link": "https://multicanais.com/"
+					},
+					{
+						"name": "Legendas.tv",
+						"link": "http://legendas.tv/"
+					},
+					{
+						"name": "1337x",
+						"link": "https://1337x.to/"
+					}
+				]
+			},
+			{
+				"name": "GitHub",
+				"link": "https://github.com/"
+			},
+			{
+				"nick": "code",
+				"name": "Code",
+				"link": [
+					{
+						"name": "git - Config",
+						"link": "https://www.shellhacks.com/git-config-username-password-store-credentials/"
+					},
+					{
+						"name": "git - Guia",
+						"link": "https://rogerdudler.github.io/git-guide/index.pt_BR.html"
+					},
+					{
+						"nick": "pradoplucasGit",
+						"name": "pradoplucasGit",
+						"link": [
+							{
+								"name": "github.io",
+								"link": "https://github.com/pradoplucas/pradoplucas.github.io"
+							},
+							{
+								"name": "sgce",
+								"link": "https://github.com/pradoplucas/sgce"
+							},
+							{
+								"name": "certificatesAPI",
+								"link": "https://github.com/pradoplucas/certificatesAPI"
+							},
+							{
+								"name": "certificados",
+								"link": "https://github.com/pradoplucas/certificados"
+							},
+							{
+								"name": "twitch",
+								"link": "https://github.com/pradoplucas/twitch"
+							}
+						]
+					},
+					{
+						"nick": "keyscapeGit",
+						"name": "keyscapeGit",
+						"link": [
+							{
+								"name": "github.io",
+								"link": "https://github.com/keyscape/keyscape.github.io"
+							},
+							{
+								"name": "keyscapeAPI",
+								"link": "https://github.com/keyscape/keyscapeAPI"
+							},
+							{
+								"name": "system",
+								"link": "https://github.com/keyscape/system"
+							},
+							{
+								"name": "10conectados",
+								"link": "https://github.com/keyscape/10conectados"
+							},
+							{
+								"name": "euclidesModule",
+								"link": "https://github.com/keyscape/euclidesModule"
+							}
+						]
+					},
+					{
+						"nick": "dqrktoGit",
+						"name": "dqrktoGit",
+						"link": [
+							{
+								"name": "favs",
+								"link": "https://github.com/dqrkto/favs"
+							}
+						]
+					},
+					{
+						"nick": "favsGit",
+						"name": "favsGit",
+						"link": [
+							{
+								"name": "originals",
+								"link": "https://github.com/pradoplucas/originals/edit/main/sites.js"
+							},
+							{
+								"name": "obfuscator.io",
+								"link": "https://obfuscator.io/"
+							},
+							{
+								"name": "favsAPI",
+								"link": "https://github.com/pradoplucas/favsAPI/edit/main/sites.js"
+							}
+						]
+					},
+					{
+						"nick": "xOutCode",
+						"name": "xOut",
+						"link": [
+							{
+								"name": "crypto123",
+								"link": "https://dqrkto.github.io/crypto123/"
+							},
+							{
+								"name": "infoProject",
+								"link": "https://heroku....."
+							},
+							{
+								"name": "MongoDB Cloud",
+								"link": "https://account.mongodb.com/account/login"
+							}
+						]
+					}
+				]
+			},
+			{
+				"nick": "codePython",
+				"name": "Code_Py",
+				"link": [
+					{
+						"name": "Requests",
+						"link": "https://docs.python-requests.org/en/master/"
+					},
+					{
+						"name": "SQLite3",
+						"link": "http://pythonclub.com.br/gerenciando-banco-dados-sqlite3-python-parte1.html"
+					},
+					{
+						"name": "IceCream",
+						"link": "https://github.com/gruns/icecream"
+					}
+				]
+			},
+			{
+				"nick": "codeWeb",
+				"name": "Code_Web",
+				"link": [
+					{
+						"name": "Heroku",
+						"link": "https://dashboard.heroku.com/apps"
+					},
+					{
+						"name": "Kaffeine",
+						"link": "https://kaffeine.herokuapp.com/"
+					},
+					{
+						"name": "MongoDB",
+						"link": "https://account.mongodb.com/"
+					},
+					{
+						"name": "Gitpod",
+						"link": "https://gitpod.io/workspaces"
+					},
+					{
+						"name": "Color Mixer",
+						"link": "https://colordesigner.io/color-mixer"
+					},
+					{
+						"nick": "webHTMLCSSJS",
+						"name": "HTML/CSS/JS",
+						"link": [
+							{
+								"name": "Scroll Indicator",
+								"link": "https://www.w3schools.com/howto/howto_js_scroll_indicator.asp"
+							},
+							{
+								"name": "Coolors",
+								"link": "https://coolors.co/"
+							},
+							{
+								"name": "Web to App",
+								"link": "https://www.androidpro.com.br/blog/desenvolvimento-android/webview-converter-site-aplicativo/"
+							},
+							{
+								"name": "positronX",
+								"link": "https://www.positronx.io/"
+							},
+							{
+								"name": "Google Style Guide",
+								"link": "https://google.github.io/styleguide/jsguide.html"
+							},
+							{
+								"name": "Airbnb Style Guide",
+								"link": "https://github.com/airbnb/javascript"
+							}
+						]
+					},
+					{
+						"nick": "webTemplates",
+						"name": "Templates",
+						"link": [
+							{
+								"name": "Start Bootstrap",
+								"link": "https://startbootstrap.com/"
+							},
+							{
+								"name": "HTML5 UP",
+								"link": "https://html5up.net/"
+							},
+							{
+								"name": "Bootstrap 5",
+								"link": "https://getbootstrap.com/docs/5.0/getting-started/introduction/"
+							},
+							{
+								"name": "Examples BS5",
+								"link": "https://getbootstrap.com/docs/5.0/examples/"
+							}
+						]
+					},
+					{
+						"nick": "webNode",
+						"name": "Node.JS",
+						"link": [
+							{
+								"name": "Curso Node.JS",
+								"link": "https://www.youtube.com/playlist?list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B"
+							},
+							{
+								"name": "Server Node.JS",
+								"link": "https://www.sitepoint.com/build-simple-beginner-app-node-bootstrap-mongodb/"
+							},
+							{
+								"name": "API Node.JS",
+								"link": "https://medium.com/trainingcenter/brincando-com-uma-api-simples-em-node-js-8c1a50c7aab3"
+							},
+							{
+								"name": "Passport.js",
+								"link": "https://www.luiztools.com.br/post/autenticacao-em-node-js-com-passport/"
+							},
+							{
+								"name": "Managers #A",
+								"link": "https://expressjs.com/en/advanced/pm.html"
+							},
+							{
+								"name": "Managers #B",
+								"link": "https://maximorlov.com/start-node-js-in-production/"
+							},
+							{
+								"name": "Systemd #A",
+								"link": "https://www.axllent.org/docs/nodejs-service-with-systemd/"
+							},
+							{
+								"name": "Systemd #B",
+								"link": "https://nodesource.com/blog/running-your-node-js-app-with-systemd-part-1/"
+							}
+						]
+					},
+					{
+						"nick": "webReact",
+						"name": "React.JS",
+						"link": [
+							{
+								"name": "Curso React JS",
+								"link": "https://www.youtube.com/playlist?list=PLdPPE0hUkt0q7bwgAlk6UimtBKoNfM1tw"
+							}
+						]
+					},
+					{
+						"nick": "webApache",
+						"name": "Apache",
+						"link": [
+							{
+								"name": "Server",
+								"link": "https://www.youtube.com/playlist?list=PLWTJMG9wMAH-mNntCsdtg9JHSpt_tmvgy"
+							},
+							{
+								"name": "Server Apache",
+								"link": "https://ubuntu.com/tutorials/install-and-configure-apache#1-overview"
+							}
+						]
+					}
+				]
+			},
+			{
+				"nick": "image",
+				"name": "Image",
+				"link": [
+					{
+						"name": "OIE",
+						"link": "https://www.online-image-editor.com/"
+					},
+					{
+						"name": "ezGIF",
+						"link": "https://ezgif.com/"
+					},
+					{
+						"name": "SmallPDF",
+						"link": "https://smallpdf.com/"
+					},
+					{
+						"name": "Pixabay",
+						"link": "https://pixabay.com/"
+					},
+					{
+						"name": "PNGWing",
+						"link": "https://www.pngwing.com/pt"
+					},
+					{
+						"name": "Pexels",
+						"link": "https://www.pexels.com/pt-br/"
+					},
+					{
+						"name": "Pxfuel",
+						"link": "https://www.pxfuel.com/"
+					},
+					{
+						"name": "Unplash",
+						"link": "https://unsplash.com/"
+					},
+					{
+						"name": "TinyJPG",
+						"link": "https://tinyjpg.com/"
+					},
+					{
+						"name": "Freepik",
+						"link": "https://www.freepik.com/"
+					},
+					{
+						"name": "Poster Burner",
+						"link": "https://www.posterburner.com/PosterBurnerSuperRez.aspx"
+					},
+					{
+						"name": "Vectorizer",
+						"link": "https://www.vectorizer.io/"
+					}
+				]
+			},
+			{
+				"nick": "books",
+				"name": "Books",
+				"link": [
+					{
+						"name": "Genesis",
+						"link": "https://libgen.is/"
+					},
+					{
+						"name": "Le Livros",
+						"link": "https://lelivros.love/"
+					},
+					{
+						"name": "Docero",
+						"link": "https://docero.com.br/"
+					},
+					{
+						"name": "Z-Library",
+						"link": "https://br1lib.org/"
+					},
+					{
+						"name": "Convert",
+						"link": "https://ebook.online-convert.com/convert-to-mobi"
+					}
+				]
+			},
+			{
+				"nick": "bills",
+				"name": "Bills",
+				"link": [
+					{
+						"name": "BrasilNet",
+						"link": "http://sistema.brasilnetcp.net.br:8080/sac/login/?sys=SAC"
+					},
+					{
+						"name": "Copel",
+						"link": "https://www.copel.com/AgenciaWeb/autenticar/loginCliente.do"
+					},
+					{
+						"name": "Sanepar",
+						"link": "https://site.sanepar.com.br/servicos/quer-pagar-sua-conta"
+					}
+				]
+			},
+			{
+				"nick": "xout",
+				"name": "xOut",
+				"link": [
+					{
+						"name": "TV Time",
+						"link": "http://tvtime.com/"
+					},
+					{
+						"name": "Canva",
+						"link": "https://www.canva.com/"
+					},
+					{
+						"name": "Programação",
+						"link": "https://redeglobo.globo.com/rpc/programacao/#202104189"
+					},
+					{
+						"name": "Agenda GE",
+						"link": "http://globoesporte.globo.com/agenda/"
+					},
+					{
+						"name": "StatCounter",
+						"link": "https://gs.statcounter.com/"
+					},
+					{
+						"name": "HowLongToBeat",
+						"link": "https://howlongtobeat.com/"
+					},
+					{
+						"name": "r/CrackWatch",
+						"link": "https://www.reddit.com/r/CrackWatch/"
+					},
+					{
+						"name": "Outline",
+						"link": "https://outline.com/"
+					},
+					{
+						"name": "scrcpy",
+						"link": "https://github.com/Genymobile/scrcpy"
+					},
+					{
+						"name": "QR Code",
+						"link": "https://www.the-qrcode-generator.com/"
+					},
+					{
+						"name": "Mussum Ipsum",
+						"link": "https://mussumipsum.com/"
+					}
+				]
+			},
+			{
+				"nick": "platforms",
+				"name": "Platforms",
+				"link": [
+					{
+						"name": "Games",
+						"link": "https://dqrkto.github.io/games/"
+					},
+					{
+						"name": "Blizzard",
+						"link": "https://www.blizzard.com/en-us/"
+					},
+					{
+						"name": "Epic",
+						"link": "https://www.epicgames.com/store/en-US/"
+					},
+					{
+						"name": "GOG",
+						"link": "https://www.gog.com/"
+					},
+					{
+						"name": "Origin",
+						"link": "https://www.origin.com/bra/en-us/store"
+					},
+					{
+						"name": "Social Club",
+						"link": "https://socialclub.rockstargames.com/"
+					},
+					{
+						"name": "Steam",
+						"link": "https://store.steampowered.com/"
+					},
+					{
+						"name": "Ubisoft",
+						"link": "https://ubisoftconnect.com/en-US/"
+					},
+
+				]
+			},
+			{
+				"nick": "projects",
+				"name": "Projects",
+				"link": [
+					{
+						"name": "Games",
+						"link": "https://dqrkto.github.io/games/"
+					},
+					{
+						"name": "Portfolio",
+						"link": "https://pradoplucas.github.io/"
+					},
+					{
+						"name": "Schedule",
+						"link": "https://gschedule.herokuapp.com/"
+					},
+					{
+						"name": "Info",
+						"link": ""
+					},
+					{
+						"name": "Accounts",
+						"link": ""
+					},
+					{
+						"nick": "accountsRead",
+						"name": "Accounts Read",
+						"link": [
+							{
+								"name": "Aline",
+								"link": "https://accountsproject.herokuapp.com/aline/"
+							},
+							{
+								"name": "Lucas",
+								"link": "https://accountsproject.herokuapp.com/lucas/"
+							},
+							{
+								"name": "Rute",
+								"link": "https://accountsproject.herokuapp.com/rute/"
+							},
+							{
+								"name": "Ygor",
+								"link": "https://accountsproject.herokuapp.com/ygor/"
+							},
+						]
+					},
+				]
+			},
+			{
+				"name": "xEdit",
+				"link": "https://github.com/dqrkto/favs/edit/main/js/in/sites.json"
+			}
+		]
+	},
+
+	"google": {
+		"name": "Google",
+		"box": "col-sm-3 col-md-2",
+		"card": "col-sm-12",
+		"content": [
+			{
+				"name": "Drive My",
+				"link": "https://drive.google.com/drive/u/0/"
+			},
+			{
+				"name": "Gmail My",
+				"link": "https://mail.google.com/mail/u/0/"
+			},
+			{
+				"name": "YouTube",
+				"link": "https://www.youtube.com/"
+			},
+			{
+				"name": "Photos",
+				"link": "https://photos.google.com/u/1/"
+			},
+			{
+				"name": "Sheet Accounts",
+				"link": "https://docs.google.com/spreadsheets/d/1yWhGiQqrJYCxHhnO2bXYfhHN4qpxsGfyQ6DsP4DTarg/edit#gid=0"
+			},
+			{
+				"nick": "maisGoogle",
+				"name": "maisGoogle",
+				"link": [
+					{
+						"name": "Sheet All",
+						"link": "https://docs.google.com/spreadsheets/d/1OWUnGBX4i-56ZPII3CZobVw_AUwqT3QJRwM1bmLxP6E/edit#gid=0"
+					},
+					{
+						"name": "Calendar",
+						"link": "https://calendar.google.com/calendar/u/0/r/"
+					},
+					{
+						"name": "Meet",
+						"link": "https://meet.google.com/landing?authuser=1"
+					},
+					{
+						"name": "Fonts",
+						"link": "https://fonts.google.com/"
+					},
+					{
+						"name": "Maps",
+						"link": "https://www.google.com/maps"
+					},
+					{
+						"name": "Keep",
+						"link": "https://keep.google.com/u/0/"
+					},
+					{
+						"name": "Scholar",
+						"link": "http://scholar.google.com/"
+					}
+				]
+			}
+		]
+	}
+}
